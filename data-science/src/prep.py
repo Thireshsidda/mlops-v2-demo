8cd98df8cde9eb3936f5a1c50773c0f3fede8881
Thiresh Sidda <92287626+Thireshsidda@users.noreply.github.com>
2025-01-21 16:45:33 +0530

    Update prep.py

--- data-science/src/prep.py
+++ data-science/src/prep.py
@@ -5,43 +5,17 @@ Prepares raw data and provides training, validation and test datasets
 """
 
 import argparse
-
 from pathlib import Path
-import os
 import numpy as np
 import pandas as pd
-
 import mlflow
 
-TARGET_COL = "cost"
+# Define the target column
+TARGET_COL = "class"
 
-NUMERIC_COLS = [
-    "distance",
-    "dropoff_latitude",
-    "dropoff_longitude",
-    "passengers",
-    "pickup_latitude",
-    "pickup_longitude",
-    "pickup_weekday",
-    "pickup_month",
-    "pickup_monthday",
-    "pickup_hour",
-    "pickup_minute",
-    "pickup_second",
-    "dropoff_weekday",
-    "dropoff_month",
-    "dropoff_monthday",
-    "dropoff_hour",
-    "dropoff_minute",
-    "dropoff_second",
-]
-
-CAT_NOM_COLS = [
-    "store_forward",
-    "vendor",
-]
-
-CAT_ORD_COLS = [
+# Define the feature columns
+FEATURE_COLS = [
+    "preg", "plas", "pres", "skin", "test", "mass", "pedi", "age"
 ]
 
 def parse_args():
@@ -50,12 +24,10 @@ def parse_args():
     parser = argparse.ArgumentParser("prep")
     parser.add_argument("--raw_data", type=str, help="Path to raw data")
     parser.add_argument("--train_data", type=str, help="Path to train dataset")
-    parser.add_argument("--val_data", type=str, help="Path to test dataset")
+    parser.add_argument("--val_data", type=str, help="Path to val dataset")
     parser.add_argument("--test_data", type=str, help="Path to test dataset")
-    
-    parser.add_argument("--enable_monitoring", type=str, help="enable logging to ADX")
+    parser.add_argument("--enable_monitoring", type=str, help="Enable logging to ADX")
     parser.add_argument("--table_name", type=str, default="mlmonitoring", help="Table name in ADX for logging")
-    
     args = parser.parse_args()
 
     return args
@@ -68,17 +40,11 @@ def log_training_data(df, table_name):
 def main(args):
     '''Read, split, and save datasets'''
 
-    # ------------ Reading Data ------------ #
-    # -------------------------------------- #
-
+    # Reading Data
     data = pd.read_csv((Path(args.raw_data)))
-    data = data[NUMERIC_COLS + CAT_NOM_COLS + CAT_ORD_COLS + [TARGET_COL]]
+    data = data[FEATURE_COLS + [TARGET_COL]]
 
-    # ------------- Split Data ------------- #
-    # -------------------------------------- #
-
-    # Split data into train, val and test datasets
-
+    # Split Data into train, val and test datasets
     random_data = np.random.rand(len(data))
 
     msk_train = random_data < 0.7
@@ -100,14 +66,10 @@ def main(args):
     if (args.enable_monitoring.lower() == 'true' or args.enable_monitoring == '1' or args.enable_monitoring.lower() == 'yes'):
         log_training_data(data, args.table_name)
 
-
 if __name__ == "__main__":
-
     mlflow.start_run()
 
-    # ---------- Parse Arguments ----------- #
-    # -------------------------------------- #
-
+    # Parse Arguments
     args = parse_args()
 
     lines = [
@@ -115,14 +77,10 @@ if __name__ == "__main__":
         f"Train dataset output path: {args.train_data}",
         f"Val dataset output path: {args.val_data}",
         f"Test dataset path: {args.test_data}",
-
     ]
 
     for line in lines:
         print(line)
     
     main(args)
-
     mlflow.end_run()
-
-    
